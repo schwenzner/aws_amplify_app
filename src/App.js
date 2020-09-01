743e643a1328a90b33a6f13d923f78400fa5a7df
Rodolph Schwenzner <rschwenzner@gmail.com>
2020-09-01 21:29:12 +0300

    Added amplify auth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./App.css";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>HELLO AWS from React App!</p>
+        <p>We now have Auth!</p>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
